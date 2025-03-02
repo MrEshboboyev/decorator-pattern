cd3fbab28f1c27137f8750771974f882c2bc8793
MrEshbobotev <mreshboboyev@gmail.com>
2025-03-02 12:58:42 +0500

    Decorator pattern in console

---
+++ src/DecoratorPatternInConsole/Decorators/Concretes/ConcreteDecoratorB.cs
@@ -0,0 +1,18 @@
+﻿using DecoratorPatternInConsole.Interfaces;
+
+namespace DecoratorPatternInConsole.Decorators.Concretes;
+
+// Decorators can execute their behavior either before or after the call to a
+// wrapped object.
+
+public class ConcreteDecoratorB : Decorator
+{
+    public ConcreteDecoratorB(Component component) : base(component)
+    {
+    }
+
+    public override string Operation()
+    {
+        return $"ConcreteDecoratorB({base.Operation()})";
+    }
+}
---
+++ src/DecoratorPatternInConsole/Interfaces/Concretes/ConcreteComponent.cs
@@ -0,0 +1,12 @@
+﻿namespace DecoratorPatternInConsole.Interfaces.Concretes;
+
+// The Concrete Component provides the default implementation of the operations.
+// There might be several variations of these classes.
+
+public class ConcreteComponent : Component
+{
+    public override string Operation()
+    {
+        return "ConcreteComponent";
+    }
+}
